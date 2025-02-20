e4947f3f04c70a54990ded66f87da3bf71319bd2
Emanu945 <emanuelbrito945@gmail.com>
2025-02-20 14:32:50 -0300

    math_plataform

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 800.0
 const JUMP_VELOCITY = -400.0
 
+@onready var wall_detector := $wall_detector as RayCast2D
 @onready var animation := $anim as AnimatedSprite2D
 
 var direction := -1
@@ -12,7 +13,18 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		animation.scale.x = direction
+		
+	if wall_detector.is_colliding():
+		direction *= -1
+		wall_detector.scale.x = -1
+	if direction == 1:
+		animation.scale.x = true
+	else:
+		animation.scale.x = false
+		
+		
 	velocity.x = direction * SPEED * delta
-
+	animation.scale.x = -direction
+	wall_detector.scale.x = -direction
 	move_and_slide()
